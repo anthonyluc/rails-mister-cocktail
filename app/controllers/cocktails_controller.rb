069dda5f6899c121d071fcba2de8f35326feb2e2
Anthony LUC <anthony.luc69@gmail.com>
2017-11-23 12:45:59 +0100

    before seed

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,22 +1,36 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :new, :create, :destroy]
+
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
-  end
-
-  def edit
+    @cocktail = Cocktail.new(cocktail_params)
   end
 
   def update
+
   end
 
   def destroy
+    @cocktail.destroy
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
